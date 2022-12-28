2ee116624d4f7f99073c410ab3794b1fc6e2c4ab
Naoyuki <zhaonzh@gmail.com>
2022-12-27 21:40:17 -0800

    navbar completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,13 +2,50 @@ import Link from 'next/link';
 
 //top navbar
 export default function NavBar() {
-    const { user, username } = {};
+    const user = null;
+    const username = null;
 
     return (<nav className="navbar">
         <ul>
             <li>
-                {/* <Link href="/"> */}
+                <Link href="/">
+                    <button className='btn-logo'>FEED</button>
+                </Link>
             </li>
+
+            {/* if user is signed in, display icon and write post button */}
+
+            {username && (
+                <>
+                    <li className='push-left'>
+                        <Link href="/admin">
+                            <button className='btn-blue'>Write Posts</button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={`/${username}`}>
+                            <img src={user?.photoURL} />
+                        </Link>
+                    </li>
+                </>
+            )}
+
+            {/* if user is not signed in, display log in button */}
+
+            {!username && (
+                <>
+                    <li>
+                        <Link href="/enter">
+                            <button className='btn-blue'>Log in</button>
+                        </Link>
+                    </li>
+                </>
+            )}
+
+
+
+
+
         </ul>
-    </nav>)
+    </nav>);
 }
